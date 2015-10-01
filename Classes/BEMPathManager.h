14c6dda411aebfb6494731959a31d08c5d0fcc15
Boris-Em <boris.emorine@gmail.com>
2015-10-01 16:20:24 -0700

    Documentation

--- Classes/BEMPathManager.h
+++ Classes/BEMPathManager.h
@@ -9,23 +9,39 @@
 @import UIKit;
 #import "BEMCheckBox.h"
 
-/*
- * Path object used by BEMCheckBox to generate paths.
+/** Path object used by BEMCheckBox to generate paths.
  */
 @interface BEMPathManager : NSObject
 
+/** The paths are assumed to be created in squares. 
+ * This is the size of width, or height, of the paths that will be created.
+ */
 @property (nonatomic) CGFloat size;
 
+/** The width of the lines on the created paths.
+ */
 @property (nonatomic) CGFloat lineWidth;
 
 /** The type of box.
- * @see BEMBoxType. */
+ * Depending on the box type, paths may be created differently
+ * @see BEMBoxType
+ */
 @property (nonatomic) BEMBoxType boxType;
 
+/** Returns a UIBezierPath object for the box of the checkbox
+ * @returns The path of the box.
+ */
 - (UIBezierPath *)pathForBox;
 
+/** Returns a UIBezierPath object for the checkmark of the checkbox
+ * @returns The path of the checkmark.
+ */
 - (UIBezierPath *)pathForCheckMark;
 
+/** Returns a UIBezierPath object for the flat checkmark of the checkbox
+ * @see BEMAnimationTypeFlat
+ * @returns The path of the flat checkmark.
+ */
 - (UIBezierPath *)pathForFlatCheckMark;
 
 @end
