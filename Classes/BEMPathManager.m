7c12b23ab935d3199a28dc222f649254a4fc33d5
Boris-Em <boris.emorine@gmail.com>
2015-10-04 09:47:26 -0700

    Added support for square box with one stroke animation

--- Classes/BEMPathManager.m
+++ Classes/BEMPathManager.m
@@ -17,15 +17,17 @@
     switch (self.boxType) {
         case BEMBoxTypeSquare:
             path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, self.size, self.size) cornerRadius:3.0];
+            [path applyTransform:CGAffineTransformRotate(CGAffineTransformIdentity, M_PI / 2)];
+            [path applyTransform:CGAffineTransformMakeTranslation(self.size, 0)];
             break;
             
         default: {
             CGFloat radius = self.size / 2;
             path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(self.size / 2, self.size / 2)
-                                                              radius: radius
-                                                          startAngle: - M_PI / 4
-                                                            endAngle:  2 * M_PI - M_PI / 4
-                                                           clockwise:YES];
+                                                  radius: radius
+                                              startAngle: - M_PI / 4
+                                                endAngle:  2 * M_PI - M_PI / 4
+                                               clockwise:YES];
         }
             break;
     }
@@ -53,16 +55,17 @@
     
     [checkMarkPath moveToPoint: CGPointMake(self.size/3.1578, self.size/2)];
     [checkMarkPath addLineToPoint: CGPointMake(self.size/2.0618, self.size/1.57894)];
-    [checkMarkPath addLineToPoint: CGPointMake(self.size/1.1553, self.size/5.9272)];
     
     if (self.boxType == BEMBoxTypeSquare) {
         // If we use a square box, the check mark should be a little bit bigger
+        [checkMarkPath addLineToPoint: CGPointMake(self.size/1.2053, self.size/4.5272)];
         [checkMarkPath applyTransform:CGAffineTransformMakeScale(1.5, 1.5)];
         [checkMarkPath applyTransform:CGAffineTransformMakeTranslation(-self.size/4, -self.size/4)];
+    } else {
+        [checkMarkPath addLineToPoint: CGPointMake(self.size/1.1553, self.size/5.9272)];
     }
     
     return checkMarkPath;
-
 }
 
 - (UIBezierPath *)pathForFlatCheckMark {
