6bd6bedecb0cc60d5214998c155aa1b213313b1c
Boris Emorine <boris.emorine@gmail.com>
2016-10-18 18:13:38 -0700

    Create new GroupTests class.

---
+++ Sample Project/CheckBoxTests/GroupTests.m
@@ -0,0 +1,41 @@
+//
+//  GroupTests.m
+//  CheckBox
+//
+//  Created by Bobo on 10/18/16.
+//  Copyright © 2016 Boris Emorine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "BEMCheckBox.h"
+
+@interface GroupTests : XCTestCase
+
+@end
+
+@implementation GroupTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
