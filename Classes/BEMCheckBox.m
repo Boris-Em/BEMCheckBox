9b7c29ce8c9d293fe0468942389dd3e033acede8
Wu Zhong <zhongwuzw@qq.com>
2017-12-06 20:06:28 +0800

    Optimize unnecessary calculation in hit test (#62)
    * optimize unnecessary calculation in hit test
    
    * update

--- Classes/BEMCheckBox.m
+++ Classes/BEMCheckBox.m
@@ -221,9 +221,13 @@
 #pragma  mark - Helper methods -
 
 #pragma mark Increase touch area
-- (BOOL) pointInside:(CGPoint)point withEvent:(UIEvent *)event;
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event;
 {
     BOOL found = [super pointInside:point withEvent:event];
+    
+    if (found) {
+        return found;
+    }
     
     CGSize minimumSize = self.minimumTouchSize;
     CGFloat width = CGRectGetWidth(self.bounds);
